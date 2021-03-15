9be095647958e811d459bc2289708ac915933f47
gmarin93 <arias.fex@gmail.com>
2021-03-15 01:31:34 -0600

    Changes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const indexControler = require('../controllers/indexController');
 
-
 router.post('/',
     indexControler.loadFile
 );
